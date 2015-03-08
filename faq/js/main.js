dd51d7db66a4d316f00badc4a12bcf695690e84e
yiyangyi <yiyangyigg@gmail.com>
2015-03-08 23:32:10 +0800

    Basic function structure constructed.

---
+++ faq/js/main.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function ($) {
+	
+	var MqM = 768, MqL = 1024;
+
+	var faqCategoriesContainer = $('.faq-categories'),
+		faqCategories = faqCategoriesContainer.find('a'),
+		faqTrigger = $('.faq-trigger'),
+		faqContainer = $('.faq-items'),
+		faqSections = $('.faq-group'),
+		closeFaqsContainer = $('.close-panel')
+
+	function closePanel(e) {
+		
+	}
+
+	function updateCategory () {
+		updateCategoryPosition();
+		updateSelectedCategory();
+	}
+
+	function updateCategoryPosition() {
+
+	}
+
+	function updateSelectedCateogry() {
+
+	}
+
+
+});
